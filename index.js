acf8e9ed864054721de6bbb1a54aacbf16b1edeb
VEB_ABHI <abhinav123venkat@gmail.com>
2024-08-08 19:42:17 +0000

    Update index,js
    added comment lines

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ homeEle = document.getElementById("home_score");
 guestEle = document.getElementById("guest_score");
 leadEle = document.getElementById("leading-team");
 mainEle = document.getElementById("main-section");
+
+//increments home values
 function incrementOfHome(count) {
 
   homeCount += count;
@@ -13,6 +15,7 @@ function incrementOfHome(count) {
   
 }
 
+// increments guest values
 function incrementOfGuest(count) {
 
   guestCount += count;
@@ -22,16 +25,20 @@ function incrementOfGuest(count) {
 
 }
 
+//Hilights the value
+
 function hilighter() {
 
   if (homeCount > guestCount) {
     homeEle.style.color = "#DC143C";
     guestEle.style.color = "#c70039";
     leadEle.textContent = "Home is leading by "+(homeCount - guestCount) + " points";
+
   } else if (guestCount > homeCount){
     guestEle.style.color = "#DC143C";
     homeEle.style.color = "#c70039";
     leadEle.textContent = "Guest is leading by "+(guestCount - homeCount)+" points";
+
   } else {
     homeEle.style.color = "#c70039"
     guestEle.style.color = "#c70039"
